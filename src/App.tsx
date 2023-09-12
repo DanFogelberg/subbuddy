9388451c01ef9537e1092d7a87dcc3fbc73e1a16
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-09-12 12:06:47 +0200

    Implementation of basic Supabase functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useId } from 'react'
 import './App.css'
 import { createClient } from '@supabase/supabase-js'
 
@@ -15,9 +15,12 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 function App() {
-  //Temporary initial fetch from Supabase
+  //Temporary testing function for Supabase
   useEffect(() => {
-    getData();
+    //getData();
+    //login();
+    //signup();
+    
   }, []);
 
   return (
@@ -28,11 +31,45 @@ function App() {
 }
 
 
-//Temporary test function for fetching data from Supabase
+//Temporary fetch test function for Supabase. Will only get subscription for user because of Supabase RLS(Row Level Security)
 async function getData() {
-  const { data } = await supabase.from("user").select();
+  const { data } = await supabase.from("subscription").select();
   console.log(data);
 
 }
+
+//Temporary login test function
+async function login() {
+  const { data, error } = await supabase.auth.signInWithPassword({
+  email: 'example2@email.com',
+  password: 'example-password2',
+  })
+  console.log(data);
+  insert();
+  getData();
+
+
+}
+
+//Temporary sign up test function
+async function signup() {
+  const { data, error } = await supabase.auth.signUp({
+  email: 'example2@email.com',
+  password: 'example-password2',
+  })
+  console.log(data);
+}
+
+//Temporary insert test function
+async function insert() {
+  const { data, error } = await supabase.from("subscription").insert({
+  user_id: ((await supabase.auth.getUser()).data.user?.id) ,
+  service_id: 1,
+  next_payment: "2000-01-01"
+  })
+  console.log(data);
+  console.log(error);
+}
+
 
 export default App
