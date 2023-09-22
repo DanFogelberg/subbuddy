c04eb7c7501e77da6bedcdda65b3e23405d8ecfc
gustavenoksson <gustav.enoksson@live.se>
2023-09-22 15:06:48 +0200

    Added components with DatePicker package
    Downloaded DatePicker package to draw calander for user to input when the payment date is.
    Updated SubscriptionButton to be able to present components on click.

--- src/components/AddSubscriptionCard/AddSubscriptionCard.tsx
+++ src/components/AddSubscriptionCard/AddSubscriptionCard.tsx
@@ -2,17 +2,17 @@ import rightArrow from '../../assets/icons/right-arrow.svg';
 
 interface AddSubscriptionCardProps {
     title: string;
-    providerImage: string;
+    image: string;
 }
 
 const AddSubscriptionCard: React.FC<AddSubscriptionCardProps> = (props) => {
     return (
-        <article className='bg-font_primary_white text-font_primary_black rounded-[10px] shadow-[rgba(0,_0,_0,_0.25)_0px_2px_2px_0px] flex justify-between items-center py-4 px-6'>
-            <div>
-                <img src={props.providerImage} alt="Image of the company." />
+        <article className='bg-font_primary_white text-font_primary_black rounded-[10px] shadow-[rgba(0,_0,_0,_0.25)_0px_2px_2px_0px] flex justify-between items-center py-4 px-6 w-full'>
+            <div className='flex items-center gap-4'>
+                <img className='w-[50px] h-[50px] rounded-[5px]' src={props.image} alt="Image of the company." />
                 <p>{props.title}</p>
             </div>
-            <img src={rightArrow} alt="Arrow pointing right." />
+            <img className='w-2 h-3' src={rightArrow} alt="Arrow pointing right." />
         </article>
     );
 }
