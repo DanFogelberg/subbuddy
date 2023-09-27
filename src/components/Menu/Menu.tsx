7c9fa2898bc7b6a17bddf9096f7c144565bc69f3
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-09-27 13:56:31 +0200

    Fixed navigation menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -12,20 +12,22 @@ const Menu = (props:MenuProps) => {
 
 
     return (
-        <nav className='bg-black flex flex-row justify-between rounded-[64px] px-10 py-1 shadow-[rgba(0,_0,_0,_0.50)_0px_0px_8px_0px] text-font_primary_white text-[10px] dark:bg-button_primary_orange'>
-            <div className='flex flex-col items-center gap-1 px-3.5 py-2' onClick = {() => props.setView("home")}>
-                <img className='h-6 w-6' src={homeIconWhite} alt="Home icon." />
-                <p className='dark:text-font_primary_black'>Hem</p>
-            </div>
-            <div className='flex flex-col items-center gap-1 px-3.5 py-2' onClick = {() => props.setView("subscriptions")}>
-                <img className='h-6 w-6' src={bellIconWhite} alt="Bell icon." />
-                <p className='dark:text-font_primary_black'>Subs</p>
-            </div>
-            <div className='flex flex-col items-center gap-1 px-3.5 py-2' onClick = {() => props.setView("profile")}>
-                <img className='h-6 w-6' src={profileIconWhite} alt="Profile icon." />
-                <p className='dark:text-font_primary_black'>Profil</p>
-            </div>
-        </nav>
+        <div className='w-full fixed bottom-5 left-0 right-0'>
+            <nav className='bg-black flex flex-row justify-between rounded-[64px] px-10 py-1 shadow-[rgba(0,_0,_0,_0.50)_0px_0px_8px_0px] text-font_primary_white text-[10px] dark:bg-button_primary_orange ml-6 mr-6'>
+                <div className='flex flex-col items-center gap-1 px-3.5 py-2' onClick = {() => props.setView("home")}>
+                    <img className='h-6 w-6' src={homeIconWhite} alt="Home icon." />
+                    <p className='dark:text-font_primary_black'>Hem</p>
+                </div>
+                <div className='flex flex-col items-center gap-1 px-3.5 py-2' onClick = {() => props.setView("subscriptions")}>
+                    <img className='h-6 w-6' src={bellIconWhite} alt="Bell icon." />
+                    <p className='dark:text-font_primary_black'>Subs</p>
+                </div>
+                <div className='flex flex-col items-center gap-1 px-3.5 py-2' onClick = {() => props.setView("profile")}>
+                    <img className='h-6 w-6' src={profileIconWhite} alt="Profile icon." />
+                    <p className='dark:text-font_primary_black'>Profil</p>
+                </div>
+            </nav>
+        </div>
     );
 };
 
